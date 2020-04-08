20d116e61b6470a4e1a4b5b481ccb5d756af9cfa
Nishant <unexme21@gmail.com>
2020-04-08 14:09:45 +0530

    Code optimization resulting in : Brasserie-App 1.1 !!!

--- Brasserie-App.py
+++ Brasserie-App.py
@@ -1,3 +1,4 @@
+#importing libraries
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 import pandas as pd
@@ -9,70 +10,51 @@ from nltk.tokenize import word_tokenize
 import sys
 
 #Linking essentials
-csvFileLink = r'C:\Users\Unex H\Desktop\Ai Project\GUI-based-Restaurant-Recommendation-System\zomato.csv'
-backgroundImageLink = r'C:\Users\Unex H\Desktop\Ai Project\GUI-based-Restaurant-Recommendation-System\images\sizller-with-noodles22.jpg'
-searchIconLink = r'C:\Users\Unex H\Desktop\Ai Project\GUI-based-Restaurant-Recommendation-System\images\icons8-search-50(2).png'
-backGround2ImageLink= r'C:\Users\Unex H\Desktop\Ai Project\GUI-based-Restaurant-Recommendation-System\images\Sitting-near-table-darkened.jpg'
-backButtonImageLink =r'C:\Users\Unex H\Desktop\Ai Project\GUI-based-Restaurant-Recommendation-System\images\icons8-back-50.png'
+csvFileLink = r'NewZomato.csv'
+backgroundImageLink = r'images\sizller-with-noodles22.jpg'
+searchIconLink = r'images\icons8-search-50(2).png'
+backGround2ImageLink= r'images\Sitting-near-table-darkened.jpg'
+backButtonImageLink =r'images\icons8-back-50.png'
 
-
+#a class to contain Restaurant Related Data
 class RestaurantData:
+        #RestaurantData Attributes
         selectedCityName=str("")
         selectedLocalityName=str("")
         selectedRestaurantName=str("")
         DataFieldWhole = pd.read_csv(csvFileLink,encoding='latin')
         tempTopRestaurantDF = None
+
         def __init__(self):
             self.tempTopRestaurantDF = self.ReturnTopFiveRestaurantName()
 
         def ReturnCityName(self):
             CityData = list(self.DataFieldWhole['City'].unique())
-            #print(CityData)
             CityData.sort()
-            #print(CityData)
             return CityData
 
-        def ReturnLocalityName(self,currentCity):#dffunc
+        def ReturnLocalityName(self,currentCity):
             self.selectedCityName = str(currentCity)
-            """
-            if(cityNameSelected == 'City'):
-                #throwError
-                print("CityName value can't be == City")
-            """
             print("SelectedCityName : {}".format(self.selectedCityName))
             DataFieldWholeTemp = self.DataFieldWhole[self.DataFieldWhole['City']==self.selectedCityName]
             localityData = list(DataFieldWholeTemp['Locality'].unique())
-            #print(localityData)
             localityData.sort()
-            #print(localityData)
             return localityData
 
         def ReturnRestaurantName(self,currentCity,currentLocality):
             self.selectedCityName = str(currentCity)
             self.selectedLocalityName = str(currentLocality)
-            """
-            if(cityNameSelected == 'City'):
-                #throwError
-                print("CityName value can't be == City")
-            """
-            #print(self.selectedCityName)
-            #print(self.selectedLocalityName)
             DataFieldWholeTemp = self.DataFieldWhole[(self.DataFieldWhole['City']==self.selectedCityName) & (self.DataFieldWhole['Locality']==self.selectedLocalityName)]
             RestaurantData = list(DataFieldWholeTemp['Restaurant Name'].unique())
-            #print(RestaurantData)
             RestaurantData.sort()
-            #print(RestaurantData)
             return RestaurantData
 
         def RestaurantRecommendFunc(self,location,title):
-            # When location comes from function ,our new data consist only location dataset
             data_sample = self.data_new_delphi.loc[self.data_new_delphi['Locality'] == location] # data frame
-            #print(data_sample)
 
             # index will be reset for cosine similarty index because Cosine similarty index has to be same value with result of tf-idf vectorize
             data_sample.reset_index(level=0, inplace=True)
             #print(data_sample)
-
 
             #Feature Extraction
             data_sample['Split']='X'
@@ -83,11 +65,9 @@ class RestaurantData:
                 split_data=' '.join(split_data[:])
                 data_sample['Split'].iloc[i]=split_data
                 
-            #print(data_sample)
             ## --- TF - IDF Vectorizer---  ##
             #Extracting Stopword
             tfidf = TfidfVectorizer(stop_words='english')
-
 
             #Replace NaN for empty string
             data_sample['Split'] = data_sample['Split'].fillna('')
@@ -99,15 +79,12 @@ class RestaurantData:
             # Using for see Cosine Similarty scores
             feature= tfidf.get_feature_names()
 
-
             ## ---Cosine Similarity--- ##()
             # Compute the cosine similarity matrix
             cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-            #print(cosine_sim)
 
             # Column names are using for index
             corpus_index=[n for n in data_sample['Split']]
-            #print(corpus_index)
 
             #Construct a reverse map of indices
             indices = pd.Series(data_sample.index, index=data_sample['Restaurant Name']).drop_duplicates()
@@ -115,8 +92,7 @@ class RestaurantData:
 
             #index of the restaurant matchs the cuisines
             idx = indices[title]
-            #print(' ')
-            #print(idx)
+
             #Aggregate rating added with cosine score in sim_score list.
             sim_scores=[]
             for i,j in enumerate(cosine_sim[idx]):
@@ -129,10 +105,8 @@ class RestaurantData:
 
             # 5 similarty cuisines
             sim_scores = sim_scores[1:6]
-            #print(sim_scores)
 
             rest_indices = [i[0] for i in sim_scores]
-            #print(rest_indices)
 
             data_x =data_sample[['Restaurant Name','Cuisines','Aggregate rating']].iloc[rest_indices]
 
@@ -147,19 +121,17 @@ class RestaurantData:
 
             #user selects City : Delhi as most Restaurants are here
             data_city =self.DataFieldWhole.loc[self.DataFieldWhole['City'] == self.selectedCityName]#user-provided-data
+            
             #Now get all the Restaurant Name, Cuisines, Locality, Aggregate rating in Delhi.
             self.data_new_delphi=data_city[['Restaurant Name','Cuisines','Locality','Aggregate rating']]
 
             #REMOVE Null values from 'Locality' column
             self.data_new_delphi['Locality'].value_counts(dropna = False).head(5)
-            #print it and try if it does what it says
 
-            #user selects it
             #selecting a locality in delhi, 
             self.data_new_delphi.loc[self.DataFieldWhole['Locality'] == 'Connaught Place']
 
             data_sample=[]
-            #print(self.RestaurantRecommendFunc('Connaught Place','Pizza Hut'))
             return self.RestaurantRecommendFunc(self.selectedLocalityName,self.selectedRestaurantName)
 
         def ReturnTopFiveRestaurantName(self):
@@ -168,11 +140,14 @@ class RestaurantData:
             restaurantTopFive.reset_index()
             return restaurantTopFive
         
+
+#a class to Create/Maintain Gui
 class Ui_MainWindow(object):
     Data = RestaurantData()
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1180, 700)
+        MainWindow.setGeometry(90,50,1180, 700)
+        #MainWindow.resize(1180, 700)
         MainWindow.setMinimumSize(QtCore.QSize(1180, 700))
         MainWindow.setMaximumSize(QtCore.QSize(1180, 700))
         MainWindow.setMouseTracking(False)
@@ -419,7 +394,7 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Brasserie"))
         
-        self.LMainBanner.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:36pt; font-weight:600; color:#55aa00;\">B</span><span style=\" font-size:36pt; font-weight:600; color:#ffffff;\">rasserie</span></p><p align=\"center\"><span style=\" font-size:18pt; font-weight:600; color:#55aa00; vertical-align:sub;\">Ai</span><span style=\" font-size:18pt; font-weight:600; color:#ffffff; vertical-align:sub;\">-powered Restaurant </span><span style=\" font-size:18pt; font-weight:600; color:#55aa00; vertical-align:sub;\">recommendation engine</span></p></body></html>"))
+        self.LMainBanner.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:36pt; font-weight:600; color:#55aa00;\">B</span><span style=\" font-size:36pt; font-weight:600; color:#ffffff;\">rasserie</span></p><p align=\"center\"><span style=\" font-size:18pt; font-weight:600; color:#55aa00; vertical-align:sub;\">AI</span><span style=\" font-size:18pt; font-weight:600; color:#ffffff; vertical-align:sub;\">-powered Restaurant </span><span style=\" font-size:18pt; font-weight:600; color:#55aa00; vertical-align:sub;\">recommendation engine</span></p></body></html>"))
         
         self.cBCity.setToolTip(_translate("MainWindow", "Enter City Name !"))
         self.cBCity.setCurrentText(_translate("MainWindow", "City"))
@@ -442,10 +417,6 @@ class Ui_MainWindow(object):
         
         self.LTopRestaurants.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:20pt; color:#1b1b1b;\">Top </span><span style=\" font-size:20pt; color:#55aa00;\">Indian Restaurants</span></p></body></html>"))
         
-        print(self.Data.ReturnTopFiveRestaurantName())
-
-        
-
         self.LRestaurantStar1.setText(_translate("MainWindow", str(self.Data.tempTopRestaurantDF['Aggregate rating'].iloc[0])))
         self.LRestaurantStar2.setText(_translate("MainWindow", str(self.Data.tempTopRestaurantDF['Aggregate rating'].iloc[1])))
         self.LRestaurantStar3.setText(_translate("MainWindow", str(self.Data.tempTopRestaurantDF['Aggregate rating'].iloc[2])))
@@ -457,6 +428,18 @@ class Ui_MainWindow(object):
         self.BRestaurant3.setText(_translate("MainWindow", self.Data.tempTopRestaurantDF['Restaurant Name'].iloc[2]))
         self.BRestaurant4.setText(_translate("MainWindow", self.Data.tempTopRestaurantDF['Restaurant Name'].iloc[3]))
         self.BRestaurant5.setText(_translate("MainWindow", self.Data.tempTopRestaurantDF['Restaurant Name'].iloc[4]))
+        
+        self.LRestaurantStar1.setToolTip(_translate("MainWindow", "Global Rating"))
+        self.LRestaurantStar2.setToolTip(_translate("MainWindow", "Global Rating"))
+        self.LRestaurantStar3.setToolTip(_translate("MainWindow", "Global Rating"))
+        self.LRestaurantStar4.setToolTip(_translate("MainWindow", "Global Rating"))
+        self.LRestaurantStar5.setToolTip(_translate("MainWindow", "Global Rating"))
+
+        self.BRestaurant1.setToolTip(_translate("MainWindow", "Top #1"))
+        self.BRestaurant2.setToolTip(_translate("MainWindow", "Top #2"))
+        self.BRestaurant3.setToolTip(_translate("MainWindow", "Top #3"))
+        self.BRestaurant4.setToolTip(_translate("MainWindow", "Top #4"))
+        self.BRestaurant5.setToolTip(_translate("MainWindow", "Top #5"))
 
         self.BRestaurant1.clicked.connect(lambda : self.TopRestaurantNamesAction(0))
         self.BRestaurant2.clicked.connect(lambda : self.TopRestaurantNamesAction(1))
@@ -466,26 +449,26 @@ class Ui_MainWindow(object):
 
     def TopRestaurantNamesAction(self,BNum):
         for i in range(5):
-            if(BNum == i): #'City','Locality','Restaurant Name'
+            if(BNum == i):
                 self.Data.selectedCityName = self.Data.tempTopRestaurantDF['City'].iloc[i]
                 self.Data.selectedLocalityName = self.Data.tempTopRestaurantDF['Locality'].iloc[i]
                 self.Data.selectedRestaurantName = self.Data.tempTopRestaurantDF['Restaurant Name'].iloc[i]
         self.ClearScreen()
 
-    def UpdateLocalityName(self):#uifunc
+    def UpdateLocalityName(self):
         self.cBlocality.clear()
         self.cBlocality.addItems(self.Data.ReturnLocalityName(self.cBCity.currentText()))
 
-    def UpdateRestaurantName(self):#uifunc
+    def UpdateRestaurantName(self):
         self.cBRestaurant.clear()
         self.cBRestaurant.addItems(self.Data.ReturnRestaurantName(self.cBCity.currentText(),self.cBlocality.currentText()))
 
-    def ShowResults(self):#uifunc
+    def ShowResults(self):
         self.Data.selectedRestaurantName = str(self.cBRestaurant.currentText())
         print("Search button Triggered...\nPrinting results...\n")
         self.CheckValues()
 
-    def CheckValues(self):#uifunc
+    def CheckValues(self):
         print("In CheckValues()...")
         print("City : ",self.Data.selectedCityName)
         print("Locality : ",self.Data.selectedLocalityName)
@@ -495,7 +478,7 @@ class Ui_MainWindow(object):
             or self.Data.selectedLocalityName == 'Locality' or self.Data.selectedLocalityName == ''\
             or self.Data.selectedRestaurantName == 'Restaurant Name' or  self.Data.selectedRestaurantName == ''):
             print("Error, unknownValues/invalidValues found")
-            #dialoog box
+            #dialog box
             msgbox = QMessageBox()
             msgbox.setWindowTitle("Invalid values")
             msgbox.setText("Please select a valid combination!")
@@ -506,7 +489,7 @@ class Ui_MainWindow(object):
             x = msgbox.exec_()
         elif(self.Data.selectedRestaurantName not in self.Data.ReturnRestaurantName(self.cBCity.currentText(),self.cBlocality.currentText())):
             print("Error, no Restaurant Found")
-            #dialoog box
+            #dialog box
             msgbox = QMessageBox()
             msgbox.setWindowTitle("No Restaurant Found")
             msgbox.setText("Error, No Restaurant Found : {}".format(self.Data.selectedRestaurantName))
@@ -519,13 +502,12 @@ class Ui_MainWindow(object):
             print("Safe To enter")
             self.ClearScreen()
 
-    def ClearScreen(self):#uifunc
+    def ClearScreen(self):
         print("Resetting All widgets")
         self.centralWidget2 = QtWidgets.QWidget(MainWindow)
         self.centralWidget2.setObjectName("centralWidget2")
         MainWindow.setCentralWidget(self.centralWidget2)
 
-        #new Nextui-Final.py Code
         self.centralWidget2.resize(1180, 700)
         self.centralWidget2.setMinimumSize(QtCore.QSize(1180, 700))
         self.centralWidget2.setMaximumSize(QtCore.QSize(1180, 700))
@@ -740,7 +722,7 @@ class Ui_MainWindow(object):
 
         if(self.Data.ReturnDF().empty == True):
             print("Sorry, no recommendation")
-            #dialoog box
+            #dialog box
             msgbox = QMessageBox()
             msgbox.setWindowTitle("No Result !")
             msgbox.setText("Sorry, no recommendation available for {}, Right Now!".format(self.Data.selectedRestaurantName))
@@ -757,6 +739,8 @@ class Ui_MainWindow(object):
         else:
             sys.exit(0)
 
+
+#main driver
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)

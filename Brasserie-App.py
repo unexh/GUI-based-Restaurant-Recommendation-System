49ff44ce960218515345e703a8709e9c8525ef3b
Nishant <unexme21@gmail.com>
2020-04-10 17:02:51 +0530

    readme.md updated

--- Brasserie-App.py
+++ Brasserie-App.py
@@ -1,3 +1,8 @@
+"""
+Code Developed and Modified By : Nishant Pandey
+Git Link : https://github.com/unexh/
+
+"""
 #importing libraries
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
@@ -769,3 +774,9 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
+
+"""
+Code Developed and Modified By : Nishant Pandey
+Git Link : https://github.com/unexh/
+
+"""
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # GUI-based-Restaurant-Recommendation-System
 A GUI based Restaurant Recommendation System using Python Programing Language.
+
+###Author : Nishant Pandey
+
+##Libraries Used:
+(Following Libraries must be installed in your sysrem in order to run the 'Brasserie-App.py' Aka Main-Implementation)
+
+1. PyQt5
+2. nltk
+3. pandas
+5. sklearn
+5. numpy
+6. sys
+
+##File to run: 
+###Brasserie.py
